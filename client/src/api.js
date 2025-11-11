ec3dd87bd81242a23298e64fb0e06bf41b00d6cc
HardikGenOne <104565179+HardikGenOne@users.noreply.github.com>
2025-11-12 01:56:34 +0530

    Add files via upload

---
+++ client/src/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const API = axios.create({
+  baseURL: "http://localhost:8080/api",
+  //https://foodify-server-9iun.onrender.com
+  // baseURL: "https://foodify-server-9iun.onrender.com/api",
+  
+
+  withCredentials: true, // send cookies
+});
+
+// signup
+export const signup = (data) => API.post("/auth/signup", data);
+
+// login
+export const login = (data) => API.post("/auth/login", data);
+
+// logged in user
+export const getUser = () => API.get("/auth/me");
+
+// logout
+export const logout = () => API.post("/auth/logout");
+ 
+
